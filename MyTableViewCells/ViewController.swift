4755cc5c4c4a7dc93f558e2923d1f28c5a169dd4
Rahul sharma <rahul.rallapalli@iconma.com>
2020-04-12 21:29:18 +0530

    Initial Commit

---
+++ MyTableViewCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTableViewCells
+//
+//  Created by Rahul Sarma  on 12/04/20.
+//  Copyright © 2020 Rahul Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
